0e4eb9ef0e344733034456c388f78e78bd7a8666
Vitaly Mykhalko <mikhalko.ukr@gmail.com>
2015-05-05 01:05:59 +0300

    updated: matrix initialization - added advanced rows

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -1,14 +1,27 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define MATRIX_SIZE 8
+
+#define ALMOST_ZERO 0.0000001
 
 #define MAX_FILENAME_SIZE 50
 #define FILENAME_TEMPLATE "data/ps/p%d"
 
 #define allocate(type, size) (type*)malloc(sizeof(type) * size)
 
+typedef struct advanced_row
+{
+	int was_sent;
+	int iters_passed;
+	int size;
+	double* row;
+} advanced_row;
+
+
+void print_advanced_row(advanced_row a_row);
 
 
 int main (int argc, char* argv[])
@@ -24,11 +37,11 @@ int main (int argc, char* argv[])
 
 	int ROW_SIZE = MATRIX_SIZE + 1;
 
-	int max_rows_in_file = (MATRIX_SIZE / PROCS_AMOUNT) + 1;
+	const int MAX_ROWS_IN_FILE = (MATRIX_SIZE / PROCS_AMOUNT) + 1;
 
 	//allocate memory for row pointers
-	double** rows = allocate(double*, max_rows_in_file);
-	for (int i = 0; i < max_rows_in_file; ++i)
+	double** rows = allocate(double*, MAX_ROWS_IN_FILE);
+	for (int i = 0; i < MAX_ROWS_IN_FILE; ++i)
 	{
 		rows[i] = NULL;
 	}
@@ -62,8 +75,8 @@ int main (int argc, char* argv[])
 		++row_number;
 	}
 
-	//print initialized rows
-	// for (int i = 0; i < max_rows_in_file; ++i)
+	// //print initialized rows
+	// for (int i = 0; i < MAX_ROWS_IN_FILE; ++i)
 	// {
 	// 	if(rows[i] == NULL)
 	// 	{
@@ -72,6 +85,37 @@ int main (int argc, char* argv[])
 	// 	printf("rank: %d, row: %d, number_1: %.3lf, number_2: %.3lf, number_-2: %.3lf, number_-2 %.3lf\n", 
 	// 		rank, i, rows[i][0], rows[i][1], rows[i][ROW_SIZE - 2], rows[i][ROW_SIZE - 1]);
 	// }
+
+	//calculate actual amount of rows
+	int rows_amount = MAX_ROWS_IN_FILE;
+	if (rows[MAX_ROWS_IN_FILE - 1] == NULL)
+	{
+		--rows_amount; 
+	}
+
+	//init advanced_rows
+	advanced_row* adv_rows = allocate(advanced_row, rows_amount);
+	for (int i = 0; i < rows_amount; ++i)
+	{
+		adv_rows[i].size = ROW_SIZE;
+		adv_rows[i].iters_passed = 0;
+		adv_rows[i].was_sent = 0;
+		adv_rows[i].row = rows[i];
+	}
+
+
+	//print advanced rows
+	for (int i = 0; i < rows_amount; ++i)
+	{
+		print_advanced_row(adv_rows[i]);
+	}
+
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	if(rank == 0) {
+		printf("all processes were initialized");
+	}
 
 
 
@@ -88,4 +132,17 @@ int main (int argc, char* argv[])
 
 	MPI_Finalize();
 	return 0;
+}
+
+void print_advanced_row(advanced_row a_row)
+{
+	printf("ADVANCED ROW:\n");
+	printf("size: %d\n", a_row.size);
+	printf("iters_passed: %d\n", a_row.iters_passed);
+	printf("was_sent: %d\n", a_row.was_sent);
+	for (int i = 0; i < a_row.size; ++i)
+	{
+		printf("%lf ", a_row.row[i]);
+	}
+	printf("\n");
 }
